5bb6e471ea012fc01befab60d0bfa325e64104ef
Марк Крылов <mark.krylov93@gmail.com>
2023-04-18 15:02:35 +0600

    models

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,44 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User
+from .models import Tag
+from .models import Task
+
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk',
+        'name',
+        'status',
+        'author',
+        'pub_date',
+    )
+    search_fields = (
+        'name',
+        'tag',
+        'pub_date',
+    )
+    list_filter = (
+        'name',
+        'tag',
+        'pub_date',
+    )
+
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk',
+        'name',
+    )
+    search_fields = (
+        'name',
+    )
+    list_filter = (
+        'name',
+    )
+    empty_value_display = '-пусто-'
+
 
 admin.site.register(User, UserAdmin)
+admin.site.register(Tag, TagAdmin)
+admin.site.register(Task, TaskAdmin)
