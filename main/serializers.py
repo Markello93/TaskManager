a798c9a978afb49960bd9ef4f46ec14d7745ad95
Марк Крылов <mark.krylov93@gmail.com>
2023-04-19 13:54:31 +0600

    add_serial_change_models

---
+++ main/serializers.py
@@ -0,0 +1,48 @@
+from rest_framework import serializers
+from .models import Tag, Task, User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = (
+            "id",
+            "username",
+            "role",
+            "first_name",
+            "last_name",
+            "email",
+            "date_of_birth",
+            "phone",
+        )
+        read_only_fields = ("id",)
+
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = (
+            "id",
+            "name",
+        )
+        read_only_fields = ("id",)
+
+
+class TaskSerializer(serializers.ModelSerializer):
+    assigned_to = UserSerializer()
+    creator = UserSerializer()
+    tags = TagSerializer()
+
+    class Meta:
+        model = Task
+        fields = (
+            "id",
+            "tags",
+            "status",
+            "pub_date",
+            "creator",
+            "assigned_to",
+            "name",
+            "description",
+        )
+        read_only_fields = ("id",)
